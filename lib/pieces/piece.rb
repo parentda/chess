2da308dbafdb07b1da6429efa4d89433f8f8ba62
parentda <danielparent21@gmail.com>
2021-10-13 19:47:25 -0400

    add to_string method

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+require 'colorize'
+
 class Piece
-  def initialize(colour)
-    @moves = 0
-    @colour = colour
+  def initialize(color)
+    @move_count = 0
+    @color = color
+  end
+
+  def to_string
+    @color == 'W' ? @symbol.white : @symbol.black
   end
 end
