f0e6faf9d354615a7a4808db39da5ad1b917c78d
parentda <danielparent21@gmail.com>
2021-10-13 19:24:42 -0400

    add Knight initialize method

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,7 +1,15 @@
-class Knight
+# frozen_string_literal: true
+require_relative 'piece'
+
+class Knight < Piece
   @@movement = [2, 1, -1, -2]
 
-  def moves
-    @@movement.permutation(2).filter { |pair| pair[0].abs != pair[1].abs }
+  attr_reader :symbol, :move_set
+
+  def initialize(color)
+    super
+    @symbol = "\u2658"
+    @move_set ||=
+      @@movement.permutation(2).reject { |pair| pair[0].abs == pair[1].abs }
   end
 end
