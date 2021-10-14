61feadb57037ac7ee5c5fb3f81d36d9264f96adb
parentda <danielparent21@gmail.com>
2021-10-14 13:29:46 -0400

    add Rook class with initialize and self.move_set methods

---
+++ lib/pieces/rook.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require_relative 'piece'
+
+class Rook < Piece
+  @@movement = [-1, 0, 1]
+
+  def initialize(color)
+    super
+    @symbol = "\u265C"
+  end
+
+  def self.move_set
+    @@move_set ||=
+      @@movement
+        .repeated_permutation(2)
+        .reject { |pair| pair[0].abs == pair[1].abs }
+        .map do |pair|
+          move_list = [pair]
+          (2..7).each { |i| move_list << pair.map { |n| n * i } }
+          move_list
+        end
+  end
+end
