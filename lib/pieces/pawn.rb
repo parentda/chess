6478923f4aa2b54ecd221fe0af1a78dad20ac38a
parentda <danielparent21@gmail.com>
2021-11-01 18:36:14 -0400

    update move_set to remove special cases; will be handled in a separate board method

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -7,26 +7,7 @@ class Pawn < Piece
   @@value = 1
 
   def self.move_set
-    @move_set ||=
-      if @color == :black
-        @@movement
-          .repeated_permutation(2)
-          .reject { |pair| pair[0] <= 0 }
-          .map do |pair|
-            move_list = [pair]
-            move_list << pair.map { |n| n * 2 } if pair[1].zero?
-            move_list
-          end
-      else
-        @@movement
-          .repeated_permutation(2)
-          .reject { |pair| pair[0] >= 0 }
-          .map do |pair|
-            move_list = [pair]
-            move_list << pair.map { |n| n * 2 } if pair[1].zero?
-            move_list
-          end
-      end
+    @move_set ||= @color == :black ? [1, 0] : [-1, 0]
   end
 
   def symbol
