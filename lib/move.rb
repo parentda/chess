4973e359d6563370d09f4433f84cc2c147bfe87f
parentda <danielparent21@gmail.com>
2021-11-12 11:42:35 -0500

    remove move.rb; functionality is included in Board

--- lib/move.rb
+++
@@ -1,9 +0,0 @@
-class Move
-  attr_reader :piece, :start_position, :end_position
-
-  def initialize(piece, start_position, end_position)
-    @piece = piece
-    @start_position = start_position
-    @end_position = end_position
-  end
-end
