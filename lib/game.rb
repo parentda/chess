47f982fb9cb0ddcb9720a75c25259d5140ea63cd
parentda <danielparent21@gmail.com>
2021-11-13 19:49:22 -0500

    add game_setup method

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,10 @@
+require_relative '../board'
+
 class Game
+  include Displayable
+
+  attr_reader :players, :current_player, :game_over, :board
+
   def initialize
     @board = Board.new
     @players = []
@@ -11,5 +17,15 @@ class Game
     game_setup
     game_loop
     game_end
+  end
+
+  def game_setup
+    return if @setup_complete
+
+    segment_break_prompt
+    introduction_prompt(@board)
+    @num_players.times { |num| create_player(num) }
+    @current_player = @players.first
+    game_start_prompt(@board, @players)
   end
 end
