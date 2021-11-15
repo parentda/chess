9f279a566d6a36e9bcdbc4454599375e1dec6397
parentda <danielparent21@gmail.com>
2021-11-15 17:34:40 -0500

    add Displayable module and restart_message and close_message methods

---
+++ lib/displayable.rb
@@ -0,0 +1,9 @@
+module Displayable
+  def restart_message
+    puts "\n\nWould you like to play again? Enter (Y/y) to start a new game/load a saved game, or any other key to quit:"
+  end
+
+  def close_message
+    puts "\nThanks for playing!"
+  end
+end
