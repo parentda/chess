6d21b671f05d19d8a4232944b8d39e6e5044d2f0
parentda <danielparent21@gmail.com>
2021-11-26 16:38:27 -0500

    add intro_message method

--- lib/displayable.rb
+++ lib/displayable.rb
@@ -1,6 +1,23 @@
 module Displayable
   SECTION_BREAK = '-' * 100
 
+  def intro_message
+    <<~HEREDOC
+    
+    #{SECTION_BREAK}
+    Welcome to #{'CHESS'.blue}!
+    
+    If you do not know the rules of chess, please familiarize yourself with them at the following link:
+
+      http://www.chessvariants.org/d.chess/chess.html
+    
+    Each turn requires two separate inputs:
+
+      1 - Coordinates of the piece to move
+      2 - Coordinates of a legal move for the selected piece
+    HEREDOC
+  end
+
   def restart_message
     puts "\n\nWould you like to play again? Enter (Y/y) to start a new game/load a saved game, or any other key to quit:"
   end
