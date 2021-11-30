6077ddf371bded96e19bf10223866cf5f4a1a811
parentda <danielparent21@gmail.com>
2021-11-30 18:49:16 -0500

    add self.game_load method

--- lib/game.rb
+++ lib/game.rb
@@ -3,19 +3,20 @@ require_relative 'display/class_displayable'
 require_relative 'display/instance_displayable'
 
 class Game
-  include Displayable
+  include InstanceDisplayable
+  extend ClassDisplayable
 
   attr_reader :players, :current_player, :game_over, :board
 
   @@saved_games_folder = 'saved_games'
 
-  def initialize
+  def initialize(game_mode)
     @board = Board.new
     @players = []
     @current_player = nil
     @game_over = false
     @game_won = false
-    @game_mode = nil
+    @game_mode = game_mode
     @setup_complete = false
   end
 
@@ -42,7 +43,7 @@ class Game
 
     display_saved_files(saved_games)
     file_num = get_file_num(saved_games)
-    puts load_game_prompt(saved_games[file_num])
+    load_game_prompt(saved_games[file_num])
     load_saved_file("#{@@saved_games_folder}/#{saved_games[file_num]}")
   end
 
@@ -69,17 +70,21 @@ class Game
       .to_i
   end
 
+  def self.game_load
+    user_input(game_load_prompt, warning_prompt_invalid, /[1-2]/)
+  end
+
   def self.game_mode
     user_input(game_mode_prompt, warning_prompt_invalid, /[1-3]/)
   end
 
   def self.create_game
-    puts intro_message
+    intro_message
     loop do
-      mode = game_mode
+      mode = game_load
       if mode == '1'
-        puts new_game_prompt
-        return new(min_word_length, max_word_length, lives)
+        new_game_message
+        return new(mode)
       else
         loaded_game = open_saved_file
         return loaded_game unless loaded_game.nil?
