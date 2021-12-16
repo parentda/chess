8727fce9801639912887403479593dd8e2d9e2bd
parentda <danielparent21@gmail.com>
2021-12-16 12:05:24 -0500

    factor segemnt_break into separate function

--- lib/display/instance_displayable.rb
+++ lib/display/instance_displayable.rb
@@ -1,4 +1,8 @@
 module InstanceDisplayable
+  def segment_break
+    puts "\n#{'-' * 100}"
+  end
+
   def restart_message
     puts "\n\nWould you like to play again? Enter (Y/y) to start a new game/load a saved game, or any other key to quit:"
   end
