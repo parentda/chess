e4344a8213e97dec803f6bc088ff9e145c4c5848
parentda <danielparent21@gmail.com>
2021-12-16 20:29:43 -0500

    add self.input_player_color method; factor out setup steps from self.create_game to self.game_setup

--- lib/game.rb
+++ lib/game.rb
@@ -16,10 +16,10 @@ class Game
 
   @@saved_games_folder = 'saved_games'
 
-  def initialize(game_mode, players = nil)
+  def initialize(game_mode, players)
     @board = Board.new
     @players = players
-    @current_player = nil
+    @current_player = @players.first
     @game_over = false
     @game_won = false
     @game_mode = game_mode
@@ -86,16 +86,17 @@ class Game
     user_input(game_mode_prompt, warning_prompt_invalid, %w[1 2 3])
   end
 
+  def self.input_player_color
+    user_input(select_color_prompt, warning_prompt_invalid, %w[1 2])
+  end
+
   def self.create_game
     segment_break
     intro_message
     loop do
       game_load = input_game_load
       if game_load == '1'
-        game_mode = input_game_mode
-        new_game_message
-        segment_break
-        return new(game_mode)
+        game_setup
       else
         loaded_game = open_saved_file
         return loaded_game unless loaded_game.nil?
@@ -104,15 +105,18 @@ class Game
   end
 
   def self.game_setup
-    # return if @setup_complete
+    game_mode = input_game_mode
 
+    if game_mode == '2'
+      player_color = input_player_color
+      game_mode = player_color == '1' ? '2' : '4'
+    end
+
+    players_list = create_players(game_mode)
+    new_game_message
     segment_break
-    introduction_prompt(@board)
-    @num_players.times { |num| create_player(num) }
-    @current_player = @players.first
-    game_start_prompt(@board, @players)
 
-    # @setup_complete = true
+    new(game_mode, players_list)
   end
 
   def self.create_players(game_mode); end
