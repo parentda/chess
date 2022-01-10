1e4c35fcc6a751f432a330b03e6b2ea843e1426d
parentda <danielparent21@gmail.com>
2022-01-10 12:52:06 -0500

    add piece_select_prompt method

--- lib/display/instance_displayable.rb
+++ lib/display/instance_displayable.rb
@@ -11,7 +11,12 @@ module InstanceDisplayable
     puts "\nYour game has been saved as:  #{filepath.blue}"
   end
 
-  def piece_select_prompt; end
+  def piece_select_prompt
+    puts <<~HEREDOC
+
+    Please input a valid piece position (e.g. a1/A1/h8/H8):
+    HEREDOC
+  end
 
   def move_select_prompt; end
 end
