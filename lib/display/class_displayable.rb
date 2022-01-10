7cac65902b4ba50f3732a20bc926cc7771decf61
parentda <danielparent21@gmail.com>
2022-01-10 13:05:12 -0500

    move restart message from instance to class

--- lib/display/class_displayable.rb
+++ lib/display/class_displayable.rb
@@ -20,6 +20,10 @@ module ClassDisplayable
     HEREDOC
   end
 
+  def restart_message
+    puts "\n\nWould you like to play again? Enter (Y/y) to start a new game/load a saved game, or any other key to quit:"
+  end
+
   def display_saved_games(hash)
     output = ''
     hash.each { |key, value| output += "#{key.to_s.blue}:  #{value}\n" }
--- lib/display/instance_displayable.rb
+++ lib/display/instance_displayable.rb
@@ -3,10 +3,6 @@ module InstanceDisplayable
     puts "\n#{'-' * 100}"
   end
 
-  def restart_message
-    puts "\n\nWould you like to play again? Enter (Y/y) to start a new game/load a saved game, or any other key to quit:"
-  end
-
   def save_game_message(filepath)
     puts "\nYour game has been saved as:  #{filepath.blue}"
   end
@@ -14,9 +10,14 @@ module InstanceDisplayable
   def piece_select_prompt
     puts <<~HEREDOC
 
-    Please input a valid piece position (e.g. a1/A1/h8/H8):
+    Please input a valid piece position:
     HEREDOC
   end
 
-  def move_select_prompt; end
+  def move_select_prompt
+    puts <<~HEREDOC
+
+    Please input a valid move position:
+    HEREDOC
+  end
 end
