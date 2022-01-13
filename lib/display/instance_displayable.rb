00d5f3c2ef9687b7d87b478984479772459bed77
parentda <danielparent21@gmail.com>
2022-01-13 12:58:13 -0500

    add promotion_prompt

--- lib/display/instance_displayable.rb
+++ lib/display/instance_displayable.rb
@@ -3,6 +3,10 @@ module InstanceDisplayable
     puts "\n#{'-' * 100}"
   end
 
+  def save_game_prompt
+    puts "\nPlease input a file name to save the game: "
+  end
+
   def save_game_message(filepath)
     puts "\nYour game has been saved as:  #{filepath.blue}"
   end
@@ -39,8 +43,21 @@ module InstanceDisplayable
   def color_prompt_message(color_prompt)
     puts <<-HEREDOC
 
-             #{color_prompt}
+              #{color_prompt}
              
+    HEREDOC
+  end
+
+  def promotion_prompt
+    puts <<~HEREDOC
+
+    Please select one of the following pieces for promotion:
+
+        #{'[1]'.blue} Queen
+        #{'[2]'.blue} Rook
+        #{'[3]'.blue} Bishop
+        #{'[4]'.blue} Knight
+
     HEREDOC
   end
 
